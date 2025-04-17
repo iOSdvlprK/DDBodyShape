d51e7d4b0e5104598c9c6855f0d78b5cf9d38646
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-04-17 23:38:35 +0900

    Initial Commit

---
+++ DDBodyShape/DDBodyShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDBodyShapeApp.swift
+//  DDBodyShape
+//
+//  Created by joe on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct DDBodyShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
