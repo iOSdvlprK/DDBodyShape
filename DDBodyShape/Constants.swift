09f0f9c70e8ec1d6140cf3c6afb92a7b4ddfe56c
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-06-19 23:40:40 +0900

    create the Splash Screen View

--- DDBodyShape/Constants.swift
+++ DDBodyShape/Constants.swift
@@ -17,3 +17,5 @@ let person1 = "pexels-heitor-verdi-2169434"
 let person2 = "pexels-ingrid-santana-2100063"
 let person3 = "pexels-italo-melo-2379005"
 let person4 = "pexels-simon-robben-614810"
+
+let bikeImage = "Bike Image"
