9ad6bbc459af48d350f2723ebc5533bfe69c1a21
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-06-21 22:34:19 +0900

    CalendarBarView: horizontal dot line

--- DDBodyShape/Results/CalendarBarView.swift
+++ DDBodyShape/Results/CalendarBarView.swift
@@ -7,12 +7,41 @@
 
 import SwiftUI
 
+struct DailyDataModel: Identifiable {
+    let id = UUID()
+    let day: String
+    let percentage: CGFloat
+    let amount: Int
+}
+
 struct CalendarBarView: View {
+    let selectedDay = "Thu"
+    let dailyData: [DailyDataModel]
+    
+    let height: CGFloat
+    let offsetY: CGFloat
+    let selectedWeight: Int
+    let selectedWeightUnits: String = "lb"
+    let selectedDim = 70.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Rectangle()
+            .stroke(style: StrokeStyle(lineWidth: 0.2, lineCap: .square, dash: [1, 5]))
+            .frame(height: 1)
     }
 }
 
 #Preview {
-    CalendarBarView()
+    CalendarBarView(
+        dailyData: [
+            .init(day: "Mon", percentage: 0.6, amount: 1),
+            .init(day: "Tue", percentage: 0.3, amount: 5),
+            .init(day: "Wed", percentage: 0.5, amount: 8),
+            .init(day: "Thu", percentage: 0.0, amount: 12),
+            .init(day: "Fri", percentage: 0.3, amount: 15)
+        ],
+        height: 100,
+        offsetY: -5,
+        selectedWeight: 13
+    )
 }
