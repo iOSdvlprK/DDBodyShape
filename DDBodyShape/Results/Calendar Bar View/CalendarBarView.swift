8d680fafad1006d47cfce08681a3fc07f8c80fab
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-06-25 23:46:51 +0900

    CalendarBarView: add VerticalDotBar

--- DDBodyShape/Results/Calendar Bar View/CalendarBarView.swift
+++ DDBodyShape/Results/Calendar Bar View/CalendarBarView.swift
@@ -25,9 +25,19 @@ struct CalendarBarView: View {
     let selectedDim = 70.0
     
     var body: some View {
-        Rectangle()
-            .stroke(style: StrokeStyle(lineWidth: 0.2, lineCap: .square, dash: [1, 5]))
-            .frame(height: 1)
+        ZStack {
+            Rectangle()
+                .stroke(style: StrokeStyle(lineWidth: 0.2, lineCap: .square, dash: [1, 5]))
+                .frame(height: 1)
+            
+            HStack(alignment: .bottom) {
+                ForEach(dailyData) { data in
+                    VerticalDotBar(percent: data.percentage, isSelected: data.day == selectedDay, height: height, day: data.day, amount: data.amount, offsetY: offsetY, selectedWeight: data.amount)
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            .offset(y: 3)
+        }
     }
 }
 
@@ -37,7 +47,7 @@ struct CalendarBarView: View {
             .init(day: "Mon", percentage: 0.6, amount: 1),
             .init(day: "Tue", percentage: 0.3, amount: 5),
             .init(day: "Wed", percentage: 0.5, amount: 8),
-            .init(day: "Thu", percentage: 0.0, amount: 12),
+            .init(day: "Thu", percentage: 1.0, amount: 12),
             .init(day: "Fri", percentage: 0.3, amount: 15)
         ],
         height: 100,
