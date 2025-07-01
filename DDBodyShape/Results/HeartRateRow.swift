b965cf2e29dbeea9767f7b47b478455d4da641d9
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-07-01 23:22:52 +0900

    complete ResultView by finishing HeartRateRow

--- DDBodyShape/Results/HeartRateRow.swift
+++ DDBodyShape/Results/HeartRateRow.swift
@@ -10,17 +10,36 @@ import SwiftUI
 struct HeartRateRow: View {
     var body: some View {
         HStack {
-            HeartBoltView()
+            HeartRate(month: "July", day: 1, year: 2025)
             
-            VStack(alignment: .leading) {
-                Text("Heart Rate")
-                Text("30 Jun 2025")
-            }
             Spacer()
             Text("256")
+                .font(.largeTitle)
+                .fontWeight(.black)
             Text("bmp")
+                .font(.subheadline)
+                .fontWeight(.bold)
         }
         .padding(.horizontal)
+    }
+}
+
+struct HeartRate: View {
+    let month: String
+    let day: Int
+    let year: Int
+    
+    var body: some View {
+        HStack {
+            HeartBoltView()
+            VStack(alignment: .leading) {
+                Text("Heart Rate")
+                    .font(.headline)
+                Text(verbatim: "\(day) \(month) \(year)")
+                    .font(.subheadline)
+                    .foregroundStyle(.gray)
+            }
+        }
     }
 }
 
